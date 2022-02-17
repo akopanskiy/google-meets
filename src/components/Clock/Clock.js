5cef1c838c09864987db8bc5e49c7d2eb4a2e50d
akopanskiy <andriikopanskiy@gmail.com>
2022-02-17 13:23:48 +0200

    added function to update clock and icons for item buttonsto componrnt controlbuttons

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Moment from 'react-moment';
 import styles from './Clock.module.css';
 
 const Clock = () => {
+  const [time, setTime] = useState(new Date());
+  const intervalID = useRef(null);
+
+  useEffect(() => {
+    intervalID.current = setInterval(() => tick(), 1000);
+  }, []);
+
+  const tick = () => {
+    setTime(new Date());
+  };
   return (
     <div className={styles.firstBlock}>
       <Moment format="HH:mm" className={styles.clock}>
-        {Date.now()}
+        {time}
       </Moment>
       <span className={styles.linkMeet}>|</span>
       <span className={styles.linkMeet}>sup-bopa-xrk</span>
--- src/components/ControlButtons/ControlButtons.js
+++ src/components/ControlButtons/ControlButtons.js
@@ -1,12 +1,30 @@
+import { AiFillAudio } from 'react-icons/ai';
+import { FiVideo } from 'react-icons/fi';
+import { MdOutlineIosShare, MdOutlineMoreVert } from 'react-icons/md';
+import { ImPhoneHangUp } from 'react-icons/im';
 import styles from './ControlButtons.module.css';
+import { IconContext } from 'react-icons';
 
 const ControlButtons = () => {
   return (
     <div className={styles.buttonsContainer}>
-      <button type="button"></button>
-      <button type="button"></button>
-      <button type="button"></button>
-      <button type="button"></button>
+      <IconContext.Provider value={{ size: '20px' }}>
+        <button type="button" className={styles.button}>
+          <AiFillAudio />
+        </button>
+        <button type="button" className={styles.button}>
+          <FiVideo />
+        </button>
+        <button type="button" className={styles.button}>
+          <MdOutlineIosShare />
+        </button>
+        <button type="button" className={styles.button}>
+          <MdOutlineMoreVert />
+        </button>
+        <button type="button" className={styles.callEndButton}>
+          <ImPhoneHangUp />
+        </button>
+      </IconContext.Provider>
     </div>
   );
 };
