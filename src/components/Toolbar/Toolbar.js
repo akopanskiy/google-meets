1bd87398817ee5fd92b3c704592d889c38042bdd
akopanskiy <andriikopanskiy@gmail.com>
2022-02-18 17:51:32 +0200

    added content and style to the bottom component Toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,49 +1,76 @@
 import { IconContext } from 'react-icons';
 
-import { AiTwotoneEdit } from 'react-icons/ai';
+import { AiTwotoneEdit, AiOutlineSetting } from 'react-icons/ai';
 import {
   MdOutlineDashboard,
   MdOutlineFullscreen,
   MdClosedCaptionOff,
 } from 'react-icons/md';
-import { BsStars } from 'react-icons/bs';
+import { BsStars, BsExclamationOctagon } from 'react-icons/bs';
+import { BiMessageError } from 'react-icons/bi';
+import { FaSearchengin } from 'react-icons/fa';
 
 import styles from './Toolbar.module.css';
 
 const Toolbar = () => {
   return (
-    <div>
-      <ul>
-        <IconContext.Provider value={{ size: '20px', color: '#696969' }}>
-          <li className={styles.toolbarItem}>
-            <AiTwotoneEdit />
-            <div className={styles.textContainer}>
-              <h5>Дошка для конференцій</h5>
-              <span>Відкрити файл Jam</span>
-            </div>
-          </li>
-          <li className={styles.toolbarItem}>
-            <MdOutlineDashboard />
-            <h5>Змінити макет</h5>
-          </li>
-          <li className={styles.toolbarItem}>
-            <MdOutlineFullscreen />
-            <h5>Повноукранний режим</h5>
-          </li>
-          <li className={styles.toolbarItem}>
-            <BsStars />
-            <h5>Застосувати візуальні ефекти</h5>
-          </li>
-          <li className={styles.toolbarItem}>
-            <MdClosedCaptionOff />
-            <div className={styles.textContainer}>
-              <h5>Субтитри</h5>
-              <span>Вимкнено</span>
-            </div>
-          </li>
-        </IconContext.Provider>
-      </ul>
-    </div>
+    <>
+      <div className={styles.itemContainerFirst}>
+        <ul>
+          <IconContext.Provider value={{ size: '20px', color: '#696969' }}>
+            <li className={styles.toolbarItem}>
+              <AiTwotoneEdit />
+              <div className={styles.textContainer}>
+                <h5>Дошка для конференцій</h5>
+                <span>Відкрити файл Jam</span>
+              </div>
+            </li>
+            <li className={styles.toolbarItem}>
+              <MdOutlineDashboard />
+              <h5>Змінити макет</h5>
+            </li>
+            <li className={styles.toolbarItem}>
+              <MdOutlineFullscreen />
+              <h5>Повноукранний режим</h5>
+            </li>
+            <li className={styles.toolbarItem}>
+              <BsStars />
+              <h5>Застосувати візуальні ефекти</h5>
+            </li>
+            <li className={styles.toolbarItem}>
+              <MdClosedCaptionOff />
+              <div className={styles.textContainer}>
+                <h5>Субтитри</h5>
+                <span>Вимкнено</span>
+              </div>
+            </li>
+          </IconContext.Provider>
+        </ul>
+      </div>
+      <div className={styles.border}></div>
+      <div className={styles.itemContainerSecond}>
+        <ul>
+          <IconContext.Provider value={{ size: '20px', color: '#696969' }}>
+            <li className={styles.toolbarItem}>
+              <BiMessageError />
+              <h5>Повідомити про проблему</h5>
+            </li>
+            <li className={styles.toolbarItem}>
+              <BsExclamationOctagon />
+              <h5>Повідомити про порушення</h5>
+            </li>
+            <li className={styles.toolbarItem}>
+              <FaSearchengin />
+              <h5>Вирішення проблем і довідка</h5>
+            </li>
+            <li className={styles.toolbarItem}>
+              <AiOutlineSetting />
+              <h5>Налаштування</h5>
+            </li>
+          </IconContext.Provider>
+        </ul>
+      </div>
+    </>
   );
 };
 export default Toolbar;
