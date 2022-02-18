9792a248f0f55a928578b5876687c782e0c7fcb4
akopanskiy <andriikopanskiy@gmail.com>
2022-02-18 18:28:50 +0200

    added component Dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import InterlocutorsWindow from './components/InterlocutorsWindow';
 import FunctionPanel from './components/FunctionPanel';
+import DropDown from './components/Dropdown/Dropdown';
 
 const App = () => {
   return (
     <>
       <InterlocutorsWindow />
       <FunctionPanel />
+      <DropDown />
     </>
   );
 };
---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,6 @@
+import styles from './Dropdown.module.css';
+
+const DropDown = () => {
+  return <div className={styles.containerDropdown}></div>;
+};
+export default DropDown;
