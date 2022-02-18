fb991d348f30ab64da79b56eb7313a16da04945a
akopanskiy <andriikopanskiy@gmail.com>
2022-02-18 13:56:10 +0200

    added style in the component Modal and added content and style to the top component Toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,10 +1,47 @@
+import { IconContext } from 'react-icons';
+
+import { AiTwotoneEdit } from 'react-icons/ai';
+import {
+  MdOutlineDashboard,
+  MdOutlineFullscreen,
+  MdClosedCaptionOff,
+} from 'react-icons/md';
+import { BsStars } from 'react-icons/bs';
+
 import styles from './Toolbar.module.css';
 
 const Toolbar = () => {
   return (
     <div>
       <ul>
-        <li>{/* <AiTwotoneEdit /> */}</li>
+        <IconContext.Provider value={{ size: '20px', color: '#696969' }}>
+          <li className={styles.toolbarItem}>
+            <AiTwotoneEdit />
+            <div className={styles.textContainer}>
+              <h5>Дошка для конференцій</h5>
+              <span>Відкрити файл Jam</span>
+            </div>
+          </li>
+          <li className={styles.toolbarItem}>
+            <MdOutlineDashboard />
+            <h5>Змінити макет</h5>
+          </li>
+          <li className={styles.toolbarItem}>
+            <MdOutlineFullscreen />
+            <h5>Повноукранний режим</h5>
+          </li>
+          <li className={styles.toolbarItem}>
+            <BsStars />
+            <h5>Застосувати візуальні ефекти</h5>
+          </li>
+          <li className={styles.toolbarItem}>
+            <MdClosedCaptionOff />
+            <div className={styles.textContainer}>
+              <h5>Субтитри</h5>
+              <span>Вимкнено</span>
+            </div>
+          </li>
+        </IconContext.Provider>
       </ul>
     </div>
   );
